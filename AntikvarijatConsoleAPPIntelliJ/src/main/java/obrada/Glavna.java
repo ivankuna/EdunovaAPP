c79a838fbaec2233d02c5256cc7fe3a7033de51b
ivankuna <ivan.kuna@yahoo.com>
2023-08-21 10:54:08 +0200

    final

--- AntikvarijatConsoleAPPIntelliJ/src/main/java/obrada/Glavna.java
+++
@@ -1,9 +0,0 @@
-package obrada;
-
-public class Glavna {
-    public static void main(String[] args) {
-        Pomocno.dev = true;
-//        Pomocno.dev = false;
-        new Izbornik();
-    }
-}
